7e1d3a25940c33c98c2c1a6f7cb70078fceaa47e
prashanth-g <prashanthcsgce@gmail.com>
2018-10-17 12:59:14 +0530

    Added server client setup

---
+++ config-client/src/main/java/com/prashanth/spring/cloud/config/client/SpringCloudConfigClient.java
@@ -0,0 +1,12 @@
+package com.prashanth.spring.cloud.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClient {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClient.class, args);
+    }
+}
